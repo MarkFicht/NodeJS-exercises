6580eea35e43383ea2b6030cd7b2ee09d591b1e5
Marek <marekficht@gmail.com>
2019-01-08 17:06:36 +0100

    Added exercises from day5

--- day_5/app/zadanieDnia1.js
+++ day_5/app/zadanieDnia1.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const app = express();
 
+let yes = 0;
+let no = 0;
+let maybe = 0;
+
 //---
 app.use( express.static('./public/zadanieDnia/') );
 
 //---
-app.get('/vote/yes', (req, res) => {
+/*app.get('/vote/yes', (req, res) => {
 
-
+    yes += 1;
+    res.send(`Dziekujemy za glos!`);
 
 });
 
-app.get('/vote/yes', (req, res) => {
+app.get('/vote/no', (req, res) => {
 
+    no += 1;
+    res.send(`Dziekujemy za glos!`);
 
+});*/
+
+app.get('/vote/:yesOrNo', (req, res) => {
+
+    if (req.params.yesOrNo === 'yes') {
+        yes += 1;
+    } else if (req.params.yesOrNo === 'no') {
+        no += 1;
+    } else if (req.params.yesOrNo === 'maybe') {
+        maybe += 1;
+    }
+
+    res.send(`Dziekujemy za glos!`);
 
 });
 
 app.get('/votes/check', (req, res) => {
 
-
+    res.send(`
+        Ilosc wszystkich glosow: ${ yes + no + maybe }\n
+        Glosy na TAK: ${ yes }\n
+        Glosy na NIE: ${ no }\n
+        Glosy na BYĆ MOŻE: ${ maybe }
+    `);
 
 });
 
